6416a11c2ddc5e7097c706a269df74e4d0d1d4b7
Nico Spiske <nico.spiske@gmail.com>
2022-10-15 17:49:48 +0200

    [minor] working SAC example from youtube video (phil), had to fix the pips because he didn't provide a requirements.txt

--- DL/main.py
+++ DL/main.py
@@ -1,26 +1,51 @@
+import pybullet_envs
 import gym
-from agent.agent import Agent
 import numpy as np
+from agent.agent import Agent
+from util.plot_learning_curve import plot_learning_curve
 
-if __name__ == "__main__":
-    env = gym.make("LunarLander-v2")
-    agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4, eps_end=0.01, input_dims=[8], lr=0.003)
-    scores, eps_history = [], []
-    n_games = 500
+if __name__ == '__main__':
+    env = gym.make('InvertedPendulumBulletEnv-v0')
+    agent = Agent(input_dims=env.observation_space.shape, env=env,
+            n_actions=env.action_space.shape[0])
+    n_games = 250
+    # uncomment this line and do a mkdir tmp && mkdir video if you want to
+    # record video of the agent playing the game.
+    #env = wrappers.Monitor(env, 'tmp/video', video_callable=lambda episode_id: True, force=True)
+    filename = 'inverted_pendulum.png'
+
+    figure_file = 'plots/' + filename
+
+    best_score = env.reward_range[0]
+    score_history = []
+    load_checkpoint = False
+
+    if load_checkpoint:
+        agent.load_models()
+        env.render(mode='human')
 
     for i in range(n_games):
-        score = 0
-        done = False
         observation = env.reset()
+        done = False
+        score = 0
         while not done:
             action = agent.choose_action(observation)
-            observation_, reward, done, truncated, info = env.step(action)
+            observation_, reward, done, info = env.step(action)
             score += reward
-            agent.store_transition(observation, action, reward, observation_, done)
-            agent.learn()
+            agent.remember(observation, action, reward, observation_, done)
+            if not load_checkpoint:
+                agent.learn()
             observation = observation_
-        scores.append(score)
-        eps_history.append(agent.epsilon)
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
 
-        avg_score = np.mean(scores[-100:])
-        print(f"episode {i}, score {score}, average score {avg_score}, epsilon {agent.epsilon}")
+        if avg_score > best_score:
+            best_score = avg_score
+            if not load_checkpoint:
+                agent.save_models()
+
+        print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % avg_score)
+
+    if not load_checkpoint:
+        x = [i+1 for i in range(n_games)]
+        plot_learning_curve(x, score_history, figure_file)
