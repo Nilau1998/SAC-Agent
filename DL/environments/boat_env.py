9f5dcde9928d08d56cb0e995a8084c2e1fed7130
Nico Spiske <nico.spiske@gmail.com>
2022-11-12 16:34:10 +0100

    [minor] checkpoint before switching Dicts to Boxes to skip env wrapper

--- DL/environments/boat_env.py
+++ DL/environments/boat_env.py
@@ -1,7 +1,5 @@
 from gym import Env
 from gym.spaces import Box, Dict
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 import random
 import math
@@ -9,14 +7,12 @@ import math
 class BoatEnv(Env):
     def __init__(self, config):
         self.config = config
-        self.current_step = 0
-        self.boat = Boat(config)
+        self.boat = Boat(self.config)
         self.reward_function = RewardFunction(
             track_width=self.config.boat_env.track_width
         )
 
         # Define action space
-        # The agent can turn the boats angle by a certain amount, nothing more
         self.min_action = -1.0
         self.max_action = 1.0
         self.action_space = Box(
@@ -25,27 +21,29 @@ class BoatEnv(Env):
             shape=(1,),
             dtype=np.float32
         )
+
         # Define observation space
-        # Observered values are the position of the boat, its angle, the wind and when it comes
         self.spaces = {
             "x_pos": Box(low=np.array([0]), high=np.array([150]), dtype=np.float32),
             "y_pos": Box(low=np.array([-5]), high=np.array([5]), dtype=np.float32),
-            "bangle": Box(low=np.array([math.radians(-90)]), high=np.array([math.radians(90)]), dtype=np.float32),
-            "wforce": Box(low=np.array([0]), high=np.array([2]), dtype=np.float32),
-            "wangle": Box(low=np.array([-1]), high=np.array([1]), dtype=np.float32)
+            "boat_angle": Box(low=np.array([math.radians(-90)]), high=np.array([math.radians(90)]), dtype=np.float32),
+            "current_wind_force": Box(low=np.array([0]), high=np.array([2]), dtype=np.float32),
+            "current_wind_angle": Box(low=np.array([-1]), high=np.array([1]), dtype=np.float32),
+            "next_wind_angle": Box(low=np.array([-1]), high=np.array([1]), dtype=np.float32)
         }
         self.observation_space = Dict(self.spaces)
 
-        # Timeout, so that the simulation doesn't run indefinitely
-        self.boat_fuel = self.config.boat_env.boat_fuel
+        # bounds = np.ones((6), dtype=np.float32)
+        # self.observation_space = Box(low=-bounds, high=bounds)
 
     # Define the step an agent can take. This means that within a step the agent can change the boats angle
     # and additionally all other calulcations happen like the wind impact.
     def step(self, action):
         # Recalc boat angle just in case (Probs not needed?), also set attributes
         self.boat.recalc_angle()
-        self.current_step += 1
-        self.boat_fuel -= 1
+        self.boat.next_wind_change()
+        self.boat.current_step += 1
+        self.boat.fuel -= 1
 
         # Apply action
         self.boat.set_velocities(action)
@@ -54,6 +52,16 @@ class BoatEnv(Env):
 
         # Finish action with boat reposition after direction change and wind application
         self.boat.set_boat_position()
+
+
+        self.state = {
+            "x_pos": self.boat.position[0],
+            "y_pos": self.boat.position[1],
+            "boat_angle": self.boat.angle,
+            "current_wind_force": self.boat.current_wind_force,
+            "current_wind_angle": self.boat.current_wind_angle,
+            "next_wind_angle": self.boat.next_wind_angle
+        }
 
         # Reward calculation
         reward = self.reward_function.linear_reward(
@@ -65,9 +73,12 @@ class BoatEnv(Env):
         # Check if goal is reached, or if the boat ran out of fuel
         # Timeout
         done = False
-        if self.boat_fuel <= 0:
+        if self.boat.fuel <= 0:
             done = True
-        info = 0
+        elif self.boat.position[0] >= 100:
+            done = True
+
+        info = {}
 
         return self.state, reward, done, info
 
@@ -75,7 +86,20 @@ class BoatEnv(Env):
         pass
 
     def reset(self):
-        pass
+        self.boat = Boat(self.config)
+
+        self.state = {
+            "x_pos": self.boat.position[0],
+            "y_pos": self.boat.position[1],
+            "boat_angle": self.boat.angle,
+            "current_wind_force": self.boat.current_wind_force,
+            "current_wind_angle": self.boat.current_wind_angle,
+            "next_wind_angle": self.boat.next_wind_angle
+        }
+
+        info = {}
+
+        return self.state, info
 
 class Boat:
     def __init__(self, config):
@@ -84,17 +108,19 @@ class Boat:
         # Env
         self.current_step = 0
         self.wind_forecast = generate_randint(self.config)
-        self.wind_forecast = np.sort(self.wind_forecast)
 
         # Boat
         self.velocity = np.array([self.config.boat_env.boat_velocity, 0], dtype=np.float32)
         self.position = np.array([0, 0], dtype=np.float32)
         self.angle = 0 # In relation to x-axis, +y = +angle, -y = -angle, based on velocity
+        self.fuel = self.config.boat_env.boat_fuel
         self.mass = 0 # Not implemented for now
 
         # Wind
-        self.wind_angle = 0 # From where the wind comes, in relation to x-axis again
-        self.wind_force = 0 # How hard the wind influences the boats angle, change happens each tick/step
+        self.current_wind_angle = 0 # From where the wind comes, in relation to x-axis again
+        self.current_wind_force = 0 # How hard the wind influences the boats angle, change happens each tick/step
+        self.next_wind_angle = 0
+        self.next_wind_force = 0
 
     def recalc_angle(self):
         """
@@ -119,26 +145,32 @@ class Boat:
         self.velocity[0] = abs_velocity * math.cos(self.angle)
         self.velocity[1] = abs_velocity * math.sin(self.angle)
 
-    def next_wind_change(self):
+    def next_wind_change(self) -> int:
         """
         Returns the amount of timesteps until the next weather event happens
         This is used to simulate the latency of the dynamic system
         This method also sets the new wind attributes so they can be applied on the ship
         """
         steps_until_wind_change = self.wind_forecast[0] - self.current_step
+        # Create initial next attributes for the first forecast
+        if self.current_step == 0:
+            self.set_wind_attributes()
+            self.wind_forecast = np.delete(self.wind_forecast, 0, 0)
+
+        # On weather forecast step, next -> current, generate new next, pop current forecast
         if self.wind_forecast[0] == self.current_step:
+            self.current_wind_angle = self.next_wind_angle
+            self.current_wind_force = self.next_wind_angle
             self.set_wind_attributes()
             self.wind_forecast = np.delete(self.wind_forecast, 0, 0)
         return steps_until_wind_change
 
     def set_wind_attributes(self):
         """
-        Sets a new set of angle and force values for the wind. This method is called
-        on every forecast step in next_wind_change().
+        Creates a new set of wind attributes for the upcoming forecast.
         """
-        self.wind_angle = random.choice([-1, 1])
-        self.wind_force = random.uniform(0, self.config.boat_env.wind_force)
-
+        self.next_wind_angle = random.choice([-1, 1])
+        self.next_wind_force = random.uniform(0, self.config.boat_env.wind_force)
 
     def apply_wind(self):
         """
@@ -148,27 +180,34 @@ class Boat:
         the challenge is increased by the wind changes that happen after every couple steps.
         """
         # Wind only affects the boat angle for now, wind comes from 90° or -90°
-        self.set_velocities(self.wind_angle * self.wind_force)
+        self.set_velocities(self.current_wind_angle * self.current_wind_force)
         self.recalc_angle()
 
 
 class RewardFunction:
+    """
+    Class that implements a bunch of reward functions that can be used to calculate the rewards
+    for the agent.
+    The reward functions need a track_width which describes the area the boat should stay in.
+    Additionally the reward functions have a x_pos_multiplier that gives extra reward the further the boat gets
+    this parameter however can be turned off by just not setting it and leaving it in its default being 0.
+    """
     def __init__(self, track_width):
         self.track_width = track_width
 
-    def linear_reward(self, position, a, x_pos_multiplier=0):
+    def linear_reward(self, position, a, x_pos_multiplier=0) -> float:
         x = position[0] * x_pos_multiplier
         y = position[1]
         linear_function = y * a
         return self.track_width - linear_function + x
 
-    def quadratic_reward(self, position, a, b, x_pos_multiplier=0):
+    def quadratic_reward(self, position, a, b, x_pos_multiplier=0) -> float:
         x = position[0] * x_pos_multiplier
         y = position[1]
         quadratic_function = math.pow(y, 2) * a + y * b
         return self.track_width - quadratic_function + x
 
-    def exponential_reward(self, position, a, x_pos_multiplier=0):
+    def exponential_reward(self, position, a, x_pos_multiplier=0) -> float:
         x = position[0] * x_pos_multiplier
         y = position[1]
         exponential_function = a * math.exp(y)
@@ -178,12 +217,15 @@ class RewardFunction:
 def generate_randint(config):
     """
     This method creates a basic np randint but makes sure there are no duplicates.
+    The point here is that having two wind changes on the same day is a bit lame.
     """
     while True:
         wind_forecast = np.random.randint( # at which step the wind should be changed/set
-                low=0,
+                low=10,
                 high=config.boat_env.boat_fuel - 15, # Little offset to make it more interesting
                 size=config.boat_env.wind_events)
+        # Check if every value is unique in the generated randint array.
         if np.sum(np.unique(wind_forecast, return_counts=True)[1]) == config.boat_env.wind_events:
+            wind_forecast = np.insert(wind_forecast, 0, 0)
             break
     return wind_forecast
