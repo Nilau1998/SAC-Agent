[minor] fixed reward function and adjusted the scaling

environment/boat_env.py
@@ -13,10 +13,7 @@ class BoatEnv(Env):
         self.action = [0]
         self.reward = 0
         self.boat = Boat(self.config)
-        self.reward_function = RewardFunction(
-            config=config,
-            a=1
-        )
+        self.reward_function = RewardFunction(config=config)
 
         self.info = {
             'termination': '',
@@ -58,16 +55,16 @@ class BoatEnv(Env):
 
         self.boat.fuel -= 1
 
-        self.boat.rudder_angle += action[0]
-        if self.boat.rudder_angle > np.pi/4:
-            self.boat.rudder_angle = np.pi/4
-        elif self.boat.rudder_angle < -np.pi/4:
-            self.boat.rudder_angle = -np.pi/4
-        self.boat.n += action[1]
-        if self.boat.n > self.config.boat.n_max:
-            self.boat.n = self.config.boat.n_max
-        elif self.boat.n < 0:
-            self.boat.n = 0
+        # self.boat.rudder_angle += action[0]
+        # if self.boat.rudder_angle > np.pi/4:
+        #     self.boat.rudder_angle = np.pi/4
+        # elif self.boat.rudder_angle < -np.pi/4:
+        #     self.boat.rudder_angle = -np.pi/4
+        # self.boat.n += action[1]
+        # if self.boat.n > self.config.boat.n_max:
+        #     self.boat.n = self.config.boat.n_max
+        # elif self.boat.n < 0:
+        #     self.boat.n = 0
 
         self.boat.run_model_step()
 
@@ -75,7 +72,9 @@ class BoatEnv(Env):
 
         # Reward calculation
         self.reward = self.reward_function.linear_reward(
-            position=[self.boat.kinematics[4], self.boat.kinematics[5]])
+            position=[self.boat.kinematics[4], self.boat.kinematics[5]],
+            x_pos_multiplier=1)
+
         self.info['episode_reward'] += self.reward
 
         done = False
@@ -156,7 +155,7 @@ class Boat:
         self.v_r_integrator.dt = self.dt
 
         # Boat
-        self.n = 0
+        self.n = 30
         self.rudder_angle = 0
 
         self.a_x = 0

environment/reward_functions.py
@@ -7,7 +7,7 @@ class RewardFunction:
     Class that implements a bunch of reward functions that can be used to calculate the rewards for the agent. The reward functions need a track_width which describes the area the boat should stay in. Additionally the reward functions have a x_pos_multiplier that gives extra reward the further the boat gets this parameter however can be turned of by just not setting it and leaving it in its default being 0.
     """
 
-    def __init__(self, config, a=0, b=0):
+    def __init__(self, config, a=1, b=1):
         self.track_width = config.boat_env.track_width
         self.track_length = config.boat_env.goal_line
         self.a = a
@@ -17,7 +17,9 @@ class RewardFunction:
         x = position[0] * x_pos_multiplier
         y = np.abs(position[1])
         linear_function = y * self.a
-        return (self.track_width - linear_function + x) / (self.track_width + self.track_length)
+        norm_linear_function = 1 - (linear_function / self.track_width)
+        norm_x = x / self.track_length
+        return norm_linear_function + norm_x
 
     def quadratic_reward(self, position, x_pos_multiplier=0):
         x = position[0] * x_pos_multiplier